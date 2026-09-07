458cdd6b4129c043fd68d197dc39d987a7544cc1
FarahAkl <farahheshamakl@gmail.com>
2026-09-08 02:29:22 +0300

    feat: add bcryptjs, express-rate-limit, and jsonwebtoken dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import expressRateLimit from "express-rate-limit";
 
 dotenv.config();
 
@@ -9,7 +10,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use(
+  expressRateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 300, 
+    standardHeaders: true,
+    legacyHeaders: false,
+  }),
+);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
