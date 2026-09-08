8ded8450b6c54ac8e5d3a18b575bd2f4040c7c77
FarahAkl <farahheshamakl@gmail.com>
2026-09-08 05:32:55 +0300

    feat: add error handling middleware and status text constants

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import express from "express";
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -8,6 +12,7 @@ import { cartRouter } from "./routes/cart.route.js";
 import { categoryRouter } from "./routes/category.route.js";
 import { orderRouter } from "./routes/order.route.js";
 import { productRouter } from "./routes/product.route.js";
+import { statusText } from "./utils/constants.js";
 
 dotenv.config();
 
@@ -33,7 +38,32 @@ app.use("/api/marketplace/cart", cartRouter);
 app.use("/api/marketplace/category", categoryRouter);
 app.use("/api/marketplace/order", orderRouter);
 app.use("/api/marketplace/product", productRouter);
-app.use('/api/profile')
+app.use("/api/profile");
+
+app.all("/*splat", (req, res, next) => {
+  res
+    .status(404)
+    .json({
+      status: statusText.ERROR,
+      message: "This resource is not available",
+    });
+});
+
+app.use(
+  (
+    error: Error & { statusCode?: number; statusText?: string },
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    res.status(error.statusCode || 500).json({
+      status: error.statusText || statusText.ERROR,
+      message: error.message || "Something is wrong!",
+      code: error.statusCode || 500,
+      data: null,
+    });
+  },
+);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}✅`);
